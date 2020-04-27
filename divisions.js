57c17bfde0f0d2ad1af2b1b429a93efb1f4ba66a
Jennylyn3 <@jhenlarcena@gmail.com>
2020-04-27 18:44:26 +0800

    third

---
+++ divisions.js
@@ -0,0 +1,31 @@
+var app = new Vue ({
+    el: '#apps',
+    data: {
+        divisions: [
+            {
+                "imgSrc": "images/women.jpg",
+                "label": "Women"
+            },
+            {
+                "imgSrc": "images/men.jpg",
+                "label": "Men"
+            },
+            {
+                "imgSrc": "images/girls.jpg",
+                "label": "Girls"
+            },
+            {
+                "imgSrc": "images/boys.jpg",
+                "label": "Boys"
+            },
+            {
+                "imgSrc": "images/toddler-girl.jpg",
+                "label": "Toddler Girl"
+            },
+            {
+                "imgSrc": "images/toddler-boy.jpg",
+                "label": "Toddler Boy"
+            }
+        ]
+}
+})
---
+++ productList.js
@@ -0,0 +1,27 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+    products: [
+        {
+            "imgSrc": "images/product1.jpg",
+            "desc": "Girlfriend Khakis in stretch Twill"
+        },
+        {
+            "imgSrc": "images/product2.webp",
+            "desc": "Vintage Wash Pocket T-shirt"
+        },
+        {
+            "imgSrc": "images/product3.jpg",
+            "desc": "Pocket T-shirt"
+        },
+        {
+            "imgSrc": "images/product4.jpg",
+            "desc": "Kids Flippy Sequin Graphic T-shirt"
+        },
+        {
+            "imgSrc": "images/product5.jpg",
+            "desc": "Short Sleeve Shirt in Linen-Cotton"
+        }
+    ]
+}
+})
